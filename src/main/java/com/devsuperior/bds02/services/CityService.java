6f53b55c3e9673529af907133b1e4cf2cf9c4bd4
ozysouza <ozysouzaa@gmail.com>
2025-07-13 02:41:15 -0400

    Create methods to cover test findAllShouldReturnAllResourcesSortedByName

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repository.CityRepository;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll(Pageable pageable) {
+        Page<City> cities = cityRepository.findAll(pageable);
+        return cities.map(CityDTO::new).getContent();
+    }
+
+}
